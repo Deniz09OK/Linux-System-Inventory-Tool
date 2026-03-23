5f65e43ec7e9bb34c1661ca694a713d850c312ab
Deniz09OK <deniz.ok@epitech.eu>
2026-03-23 22:04:34 +0100

    feat(core): ajouter l'extraction du cpu, des processus et de l'arborescence

--- Vagrantfile
+++ Vagrantfile
@@ -3,7 +3,6 @@ Vagrant.configure("2") do |config|
   config.vbguest.auto_update = false
   config.vm.provision "shell", inline: <<-SHELL
     apt-get update
-    apt-get install -y python3 python3-pip
-    apt-get install -y nano
+    apt-get install -y python3 python3-pip nano tree
   SHELL
 end
--- lsit.py
+++ lsit.py
@@ -1,3 +1,5 @@
+import subprocess
+
 with open("/etc/hostname") as f:
     hostname = f.read().strip()
 
@@ -11,6 +13,30 @@ with open("/proc/meminfo") as f:
 
 print(f"Mémoire totale : {ram_info}")
 
-with open("rapport_lsit.txt", "a") as rapport:
-    rapport.write(f"La cible a été identifiée. Nom de la machine : {hostname}\n")
-    rapport.write(f"Mémoire totale : {ram_info}\n")
+cpu_info = "Inconnu"
+with open("/proc/cpuinfo", "r") as f:
+    for ligne in f:
+        if "model name" in ligne:
+            cpu_info = ligne.split(":")[1].strip()
+            break
+
+cmd_ps = subprocess.run(["ps", "aux"], capture_output=True, text=True)
+processus_actifs = cmd_ps.stdout
+
+cmd_tree = subprocess.run(["tree", "-L", "2", "/home/vagrant"], capture_output=True, text=True)
+arborescence = cmd_tree.stdout
+
+with open("rapport_lsit.txt", "a") as f:
+    f.write(f"La cible a été identifiée. Nom de la machine : {hostname}\n")
+    f.write(f"Mémoire totale : {ram_info}\n")
+    f.write(f"Modèle du CPU : {cpu_info}\n")
+
+    f.write("\n===================================\n")
+    f.write("        PROCESSUS ACTIFS           \n")
+    f.write("===================================\n")
+    f.write(processus_actifs)
+
+    f.write("\n===================================\n")
+    f.write("      ARBORESCENCE DOSSIERS        \n")
+    f.write("===================================\n")
+    f.write(arborescence)
