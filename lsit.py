eb663da7100683de309a9ec32773d4787b38bbc4
Deniz09OK <deniz.ok@epitech.eu>
2026-03-24 18:38:53 +0100

    feat(cli): ajouter les arguments utilisateur et l'export json

--- lsit.py
+++ lsit.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 import subprocess
+import argparse
+import json
+from datetime import datetime
+
+parser = argparse.ArgumentParser(description="LSIT - Linux System Inventory Tool : Cartographie l'infrastructure locale.")
+parser.add_argument("-v", "--version", action="version", version="LSIT v1.0")
+parser.add_argument("--format", choices=["txt", "json"], default="txt", help="Format de sortie du rapport")
+args = parser.parse_args()
 
 with open("/etc/hostname") as f:
     hostname = f.read().strip()
@@ -27,17 +35,38 @@ processus_actifs = cmd_ps.stdout
 cmd_tree = subprocess.run(["tree", "-L", "2", "/home/vagrant"], capture_output=True, text=True)
 arborescence = cmd_tree.stdout
 
-with open("rapport_lsit.txt", "a") as f:
-    f.write(f"La cible a été identifiée. Nom de la machine : {hostname}\n")
-    f.write(f"Mémoire totale : {ram_info}\n")
-    f.write(f"Modèle du CPU : {cpu_info}\n")
+date_audit = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    f.write("\n===================================\n")
-    f.write("        PROCESSUS ACTIFS           \n")
-    f.write("===================================\n")
-    f.write(processus_actifs)
+if args.format == "json":
+    donnees_audit = {
+        "date": date_audit,
+        "machine": hostname,
+        "ram": ram_info,
+        "cpu": cpu_info,
+        "processus": processus_actifs,
+        "arborescence": arborescence
+    }
 
-    f.write("\n===================================\n")
-    f.write("      ARBORESCENCE DOSSIERS        \n")
-    f.write("===================================\n")
-    f.write(arborescence)
+    with open("rapport_lsit.json", "w") as f:
+        json.dump(donnees_audit, f, indent=4)
+
+    print("Rapport JSON généré avec succès !")
+
+else:
+    with open("rapport_lsit.txt", "a") as f:
+        f.write(f"Date de l'audit : {date_audit}\n")
+        f.write(f"La cible a été identifiée. Nom de la machine : {hostname}\n")
+        f.write(f"Mémoire totale : {ram_info}\n")
+        f.write(f"Modèle du CPU : {cpu_info}\n")
+
+        f.write("\n===================================\n")
+        f.write("        PROCESSUS ACTIFS           \n")
+        f.write("===================================\n")
+        f.write(processus_actifs)
+
+        f.write("\n===================================\n")
+        f.write("      ARBORESCENCE DOSSIERS        \n")
+        f.write("===================================\n")
+        f.write(arborescence)
+
+    print("Rapport TXT généré avec succès !")
